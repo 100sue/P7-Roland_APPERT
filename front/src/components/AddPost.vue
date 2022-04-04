e3266fd904123a2d7ce51a3709861719ab2ce9c5
appert <scoreur@gmail.com>
2022-04-04 13:21:21 +0200

    remis en forme du code avt soutenance

--- front/src/components/AddPost.vue
+++ front/src/components/AddPost.vue
@@ -1,158 +1,163 @@
 <template>
-    <section class="post-container">
-
-        <form class="post-card" v-on:submit.prevent="envoiForm($event)" >
-            <h1>Exprimez-vous...</h1>
-            <!-- Nouvelle publication : texte -->
-            <div class="post-infos">
-                <label for="postTitle" class="hidden">Nouvelle publication</label>
-                <textarea name="postTitle" id="postTitle" placeholder="Que voulez-vous partager ?" v-model="postForm.message" aria-invalid="false" required></textarea>
-            </div>
-            <!-- Nouvelle publication : lien -->
-            <div class="post-content">
-                <input type="text" name="postContent" id="postContent" class="post-input" placeholder="Joindre un lien ?" title="Joindre un lien"
-                    v-model="postForm.link"
-                >
-                <!-- Nouvelle publication : image -->
-                <div class="post-img">
-                    <label for="addContent"><i class="far fa-file-image" title="Ajouter un fichier"></i></label>
-                    <input type="file"
-                        id="addContent"
-                        name="image"
-                        accept="image/*"
-                        
-                    >
-                </div>
-                <button class="post-btn" title="Valider la publication">Publier</button>
-            </div>
-        </form>            
-        
-    </section>
+  <section class="post-container">
+    <form class="post-card" v-on:submit.prevent="envoiForm($event)">
+      <h1>Exprimez-vous...</h1>
+      <!-- Nouvelle publication : texte -->
+      <div class="post-infos">
+        <label for="postTitle" class="hidden">Nouvelle publication</label>
+        <textarea
+          name="postTitle"
+          id="postTitle"
+          placeholder="Que voulez-vous partager ?"
+          v-model="postForm.message"
+          aria-invalid="false"
+          required
+        ></textarea>
+      </div>
+      <!-- Nouvelle publication : lien -->
+      <div class="post-content">
+        <input
+          type="text"
+          name="postContent"
+          id="postContent"
+          class="post-input"
+          placeholder="Joindre un lien ?"
+          title="Joindre un lien"
+          v-model="postForm.link"
+        />
+        <!-- Nouvelle publication : image -->
+        <div class="post-img">
+          <label for="addContent"
+            ><i class="far fa-file-image" title="Ajouter un fichier"></i
+          ></label>
+          <input type="file" id="addContent" name="image" accept="image/*" />
+        </div>
+        <button class="post-btn" title="Valider la publication">Publier</button>
+      </div>
+    </form>
+  </section>
 </template>
-
 
 <script>
 export default {
-     name: 'AddPost',
-     props: {
-            createPost: Function
-        },
-        data() {
-            return {
-                postForm: {
-                    message: '',
-                    media: '',
-                    link: ''
-                }
-            }
-        },
-        methods: {
-            // Ajout d'une nouvelle publication 
-            envoiForm(event) { 
-                const message = this.postForm.message;
-                const media = event.target.image.files[0];
-                const link = this.postForm.link;
-                // Formulaire contenant les informations du post
-                var formData = new FormData();
-                    formData.append('message', message);
-                    formData.append('image', media);    
-                    formData.append('link', link);
-                    alert("Publication ajoutée !")
-                    this.createPost(formData);
-                    // Clean des inputs une fois la publication ajoutée
-                    this.postForm = {
-                        message: '',
-                        media: '',
-                        link: ''
-                    }
-                    event.target.image.value = ''
-                },
-            }
-        }
-    
+  name: "AddPost",
+  props: {
+    createPost: Function,
+  },
+  data() {
+    return {
+      postForm: {
+        message: "",
+        media: "",
+        link: "",
+      },
+    };
+  },
+  methods: {
+    // Ajout d'une nouvelle publication
+    envoiForm(event) {
+      const message = this.postForm.message;
+      const media = event.target.image.files[0];
+      const link = this.postForm.link;
+      // Formulaire contenant les informations du post
+      var formData = new FormData();
+      formData.append("message", message);
+      formData.append("image", media);
+      formData.append("link", link);
+      alert("Publication ajoutée !");
+      this.createPost(formData);
+      // Clean des inputs une fois la publication ajoutée
+      this.postForm = {
+        message: "",
+        media: "",
+        link: "",
+      };
+      event.target.image.value = "";
+    },
+  },
+};
 </script>
 
-
-<style scoped> 
+<style scoped>
 .post-container {
-        width: 50%;
-        margin: auto;
-        height: auto;
-        margin-bottom: 3%;
-}  
+  width: 50%;
+  margin: auto;
+  height: auto;
+  margin-bottom: 3%;
+}
 .post-card {
-        box-shadow: 0 0 20px #000000b3;
-        background: #315EF0;
-        border-radius: 5px;
-}  
+  box-shadow: 0 0 20px #000000b3;
+  background: #315ef0;
+  border-radius: 5px;
+}
 .hidden {
-        display: none;
+  display: none;
 }
 h1 {
-        padding-top: 2%;
-        padding-left: 4%;
-        color: white;
+  padding-top: 2%;
+  padding-left: 4%;
+  color: white;
 }
 .post-infos {
-        text-align: center;
+  text-align: center;
 }
 textarea {
-        width: 90%;
-        height: 5em;
-        padding: 1%;
-        margin-bottom: 2%;
-        resize: none;
+  width: 90%;
+  height: 5em;
+  padding: 1%;
+  margin-bottom: 2%;
+  resize: none;
 }
 .post-content {
-        display: flex;
-        justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
 }
 .post-input {
-        width: 40%;
-        margin-left: 4%;
-        margin-bottom: 2%;
-        padding: 1%;
-        background: #819bee;
+  width: 40%;
+  margin-left: 4%;
+  margin-bottom: 2%;
+  padding: 1%;
+  background: #819bee;
 }
 .post-img > input {
-        display: none;
+  display: none;
 }
 .post-img {
-        width: 10%;
-        font-size: 1.5em;
-        justify-content: center;
-        align-content: center;
-        display: flex;
-        padding-top: 2%;
-        color: white;
-        transition: transform .2s;
+  width: 10%;
+  font-size: 1.5em;
+  justify-content: center;
+  align-content: center;
+  display: flex;
+  padding-top: 2%;
+  color: white;
+  transition: transform 0.2s;
 }
 .post-img:hover {
-        transform: scale(1.1);
+  transform: scale(1.1);
 }
 .post-btn {
-        margin-right: 4%;
-        margin-bottom: 2%;
-        width: 40%;
-        background-color: #FD340A;
-        border: 1px solid #D4D3E8;
-        box-shadow: 0px 1px 1px black;
-        font-weight: 700;
-        color: white;
-        height: 3em;
-        border-radius: 10px;
+  margin-right: 4%;
+  margin-bottom: 2%;
+  width: 40%;
+  background-color: #fd340a;
+  border: 1px solid #d4d3e8;
+  box-shadow: 0px 1px 1px black;
+  font-weight: 700;
+  color: white;
+  height: 3em;
+  border-radius: 10px;
 }
 /* Medium devices (tablets, 768px and up) */
 @media screen and (max-width: 1023px) {
-.post-container {
-            width: 60%;
-        }
- } 
+  .post-container {
+    width: 60%;
+  }
+}
 /* Small device (smartphone, to 767px max) */
 @media screen and (max-width: 767px) {
-.post-container {
-            width: 80%;
-            margin-bottom: 8%;
-    }
+  .post-container {
+    width: 80%;
+    margin-bottom: 8%;
+  }
 }
 </style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,24 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
+const app = createApp(App);
 
-const app = createApp(App)
-
-app.directive('click-outside', {
-    mounted(el, binding, vnode) {
-        el.clickOutsideEvent = function (event) {
-            if (!(el == event.target || el.contains(event.target))) {
-              vnode.context[binding.expression](event, el);
-            }
-          };
-          document.body.addEventListener('click', el.clickOutsideEvent)
-        },
-        unbind: function (el) {
-          document.body.removeEventListener('click', el.clickOutsideEvent)
-        },
+app.directive("click-outside", {
+  mounted(el, binding, vnode) {
+    el.clickOutsideEvent = function (event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event, el);
+      }
+    };
+    document.body.addEventListener("click", el.clickOutsideEvent);
+  },
+  unbind: function (el) {
+    document.body.removeEventListener("click", el.clickOutsideEvent);
+  },
 });
 
-
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.mount("#app");
